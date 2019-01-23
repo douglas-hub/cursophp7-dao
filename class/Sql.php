81f21cb2a4fd9371fb69ae7169b179e51c62a76d
Douglas Ferreira <douglasferreira@hotmail.com>
2019-01-23 18:38:59 -0200

    atualizei

--- class/Sql.php
+++ class/Sql.php
@@ -8,7 +8,7 @@ public function __construct(){
 }
 public function setParams($statment , $parameters = array()){
 	foreach($parameters as $key =>$value){
-		$this->setParam($key , $value);
+		$this->setParam($statment,$key , $value);
 	}
 }
 private function setParam($statment , $key , $value){
--- class/Usuario.php
+++ class/Usuario.php
@@ -1,6 +1,67 @@
 <?php
 class Usuario {
-	
+	private $idusuario ;
+	private $deslogin ;
+	private $dessenha ;
+	private $dtcadastro ;
+
+	public function getUser($id){
+		$sql = new Sql();
+
+		$result = $sql->select(" SELECT * FROM tb_usuarios WHERE idusuario = :ID " , array(
+			":ID"=>$id 
+		));
+
+		if(isset($result)){
+			$row = $result[0];
+
+			$this->setId($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}
+
+	}
+
+	public function __toString(){
+
+		return json_encode(array(
+			'idusuario' => $this->getId(),
+			'deslogin' => $this->getDeslogin(),
+			'dessenha' => $this->getDessenha(),
+			'dtcadastro' => $this->getDtcadastro()->format("d/m/Y")
+		));
+
+	}
+
+
+//---------------------------GETTER E SETTER-----------------------------------------
+	public function getId(){
+		return $this->idusuario ;
+	}
+	public function setId($value){
+		$this->idusuario = $value ;
+	}
+	public function getDeslogin(){
+		return $this->deslogin ;
+	}
+	public function setDeslogin($value){
+		$this->deslogin = $value ;
+	}
+	public function getDessenha(){
+		return $this->dessenha ;
+	}
+	public function setDessenha($value){
+		$this->dessenha = $value ;
+	}
+	public function getDtcadastro(){
+		return $this->dtcadastro ;
+	}
+	public function setDtcadastro($value){
+		$this->dtcadastro = $value ;
+	}
+
+//--------------------------------------------------------------------
 }
 
-?>
+
